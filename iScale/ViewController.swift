88587fe8170f5abb62b52f22f31618e978205c74
pjmercurio <paulmercurio@berkeley.edu>
2016-05-31 14:59:07 -0700

    Added icons

--- iScale/ViewController.swift
+++ iScale/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     var tareWeights: [Float] = [0.0, 0.0, 0.0, 0.0];
     var weights: [Float] = [0.0, 0.0, 0.0, 0.0];
     let units = ["mg", "g", "Oz", "Lbs"];
+    let labels = [["",""], ["180", "360"], ["6.5", "13"], ["0.4", "0.8"]];
     var segIndex: Int = 0;
 
+    
+    @IBOutlet weak var halfwayLabel: UILabel!
+    @IBOutlet weak var fullwayLabel: UILabel!
     @IBOutlet weak var tareButton: UIButton!
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var unitSelector: UISegmentedControl!
@@ -21,6 +25,8 @@ class ViewController: UIViewController {
     @IBAction func unitChanged(sender: AnyObject) {
         segIndex = unitSelector.selectedSegmentIndex;
         weightLabel.text = "\(weights[segIndex]) \(units[segIndex])";
+        halfwayLabel.text = labels[segIndex][0];
+        fullwayLabel.text = labels[segIndex][1];
     }
     @IBAction func tare(sender: AnyObject) {
         tareWeights[0] = weights[0];
@@ -39,11 +45,12 @@ class ViewController: UIViewController {
         return UIStatusBarStyle.LightContent;
     }
     
+    // Adjust weight as it changes
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if let touch = touches.first {
             if (touch.view != tareButton) {
-                if traitCollection.forceTouchCapability == UIForceTouchCapability.Available {
-                    if touch.force >= touch.maximumPossibleForce {
+                if traitCollection.forceTouchCapability == UIForceTouchCapability.Available { // See if device supports force touch
+                    if touch.force >= touch.maximumPossibleForce { // Max weight exceeded
                         weightLabel.text = "ERROR";
                         weightLabel.textColor = UIColor.redColor();
                     }
@@ -68,11 +75,12 @@ class ViewController: UIViewController {
         }
     }
     
+    // Nothing on the scale anymore, reset all values
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if let touch = touches.first {
             if (touch.view != tareButton) {
                 
-                if (weights[1] > 250) {
+                if (weights[1] > 250) { // Don't want needle to take the "shortcut route" back
                     let animation = CAKeyframeAnimation.init(keyPath: "transform.rotation.z");
                     animation.duration = 1.0;
                     animation.removedOnCompletion = false;
@@ -84,12 +92,12 @@ class ViewController: UIViewController {
                     animation.timingFunctions = [CAMediaTimingFunction.init(name: kCAMediaTimingFunctionLinear), CAMediaTimingFunction.init(name: kCAMediaTimingFunctionLinear)];
                     needle.layer.addAnimation(animation, forKey: "transform.rotation.z");
                 }
-                    
                 else {
                     UIView.animateWithDuration(1.0, animations: {
                         self.needle.transform = CGAffineTransformMakeRotation(0.0);
                     })
                 }
+                
                 weightLabel.text = "0.0 \(units[segIndex])";
                 weightLabel.textColor = UIColor.whiteColor();
                 weights = [0.0, 0.0, 0.0, 0.0];
